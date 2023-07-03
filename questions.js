0eaf86481775e18489bcf31ef4667908c3f2fd10
LuchoCruz08 <110788458+LuchoCruz08@users.noreply.github.com>
2023-07-03 20:39:04 -0300

    Complete website

---
+++ questions.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+            number:1,        
+            question:'¿Cuál es la capital de Argentina?', 
+            answer:'Buenos Aires', 
+            options:[
+              'Buenos Aires', 
+              'Rosario', 
+              'Cordoba', 
+              'La Plata'
+            ]
+    }, 
+    {
+            number:2,
+            question: '¿Cuál es la provincia con más habitantes en Argentina?',
+            answer: 'Provincia de Buenos Aires',
+            options: [
+              'Provincia de Santa Fe',
+              'Provincia de Cordoba',
+              'Provincia de Mendoza',
+              'Provincia de Buenos Aires'
+            ]
+    }, 
+    {
+            number:3,
+            question: '¿Cuál es la capital con menos habitantes en Argentina?',
+            answer: 'Provincia de Tierra del Fuego',
+            options: [
+              'Provincia de Santa Cruz',
+              'Provincia de Tierra del Fuego',
+              'Provincia de San Luis',
+              'Provincia de La Pampa'
+            ]
+    }, 
+    {
+            number:4,
+            question: '¿Con cuántos países limita Argentina?',
+            answer: '5',
+            options: [
+              '3',
+              '4',
+              '5',
+              '6'
+            ]
+    }, 
+    {
+            number:5,
+            question:'¿Cuántas regiones hay en Argentina?',
+            answer:'8',
+            options:[
+              '6',
+              '7',
+              '8',
+              '9'
+            ]
+    }, 
+ ]
